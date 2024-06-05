1444da09a97529f71a8145934726a561a0637198
Daniel Abraham <daniel@autokitteh.com>
2024-06-05 19:46:37 +0300

    autokitteh 0.6.4 (#24)
    Created with `brew bump-formula-pr`.<details>
      <summary>release notes</summary>
      <pre>## What's Changed
    * feat(env)!: non-optional project by @ig-ra in
    https://github.com/autokitteh/autokitteh/pull/349
    * ENG-894: load connection variables into env by @itayd in
    https://github.com/autokitteh/autokitteh/pull/347
    * [ENG-892] Allow users to configure which Python to use by @tebeka in
    https://github.com/autokitteh/autokitteh/pull/339
    * fix: resolve github vars from secrets by @ig-ra in
    https://github.com/autokitteh/autokitteh/pull/351
    * Use repr to represent function call args by @tebeka in
    https://github.com/autokitteh/autokitteh/pull/352
    * ENG-960: fix count only sessions not returning any results by
    @efiShtain in https://github.com/autokitteh/autokitteh/pull/353
    * fix (sdk): unwrapping embedded time.Time by @ig-ra in
    https://github.com/autokitteh/autokitteh/pull/354
    * feat: scheduler workflow by @ig-ra in
    https://github.com/autokitteh/autokitteh/pull/331
    * ENG-967: fix invalid login url when opening browser from cli by
    @efiShtain in https://github.com/autokitteh/autokitteh/pull/355
    * fix: wrap only exported time by @ig-ra in
    https://github.com/autokitteh/autokitteh/pull/356
    * [ENG-936] Create `autokitteh` PyPI package by @tebeka in
    https://github.com/autokitteh/autokitteh/pull/357
    
    
    **Full Changelog**:
    https://github.com/autokitteh/autokitteh/compare/v0.6.3...v0.6.4</pre>
    </details>

--- Formula/autokitteh.rb
+++ Formula/autokitteh.rb
@@ -1,8 +1,8 @@
 class Autokitteh < Formula
   desc "Durable workflow automation made simple"
   homepage "https://autokitteh.com"
-  url "https://github.com/autokitteh/autokitteh/archive/refs/tags/v0.6.2.tar.gz"
-  sha256 "c4b7c9a8e479bfdf9b33ef74d81a9d054d9d625ab00527905b583c6a30f6464b"
+  url "https://github.com/autokitteh/autokitteh/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "417cd1c2f5ba3a562790a1ba8b629bef20a08e78671717f6f321195d72cd1016"
   license "Apache-2.0"
   head "https://github.com/autokitteh/autokitteh.git", branch: "main"
 
